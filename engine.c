bb7e32ec2e0f0e339b925e3c43da037398477bec
spaghettyii <hyunwooyi2@gmail.com>
2025-08-15 16:45:20 -0300

    made some definitions in the header.h file

--- engine.c
+++ engine.c
@@ -1,22 +1,39 @@
+/**
+ * Intro:
+ * 	Hello! This is my personal project on making a chess engine
+ * I will be loosly following a guide and will try to make this
+ * the way I want to. This is technically my first C project
+ * that I am working on myself and it seems pretty interesting 
+ * so I decided to do it. 
+ * 
+ * Representations:
+ * 	Chess requires a way to represent:
+ *		1. the board = int array
+ * 		2. the pieces = different ints represent different pieces
+ *		3. who's turn it is (side)
+ *		4. the number of "plays" (half-moves) made in the game
+ * 		5. other rule related things
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "header.h"
 
-typedef int* board_t;
-
-enum side {WHITE, BLACK}; 
 
 void initialize(board_t board)
 {
-
+	
 	return;
 }
 
 int main(int argc, char **argv)
 {
-	int *board = calloc(120, sizeof(int));
+	board_t board = calloc(BRD_SQ_NUM, sizeof(int));
 	int fiftyMoves = 0;
 	int threeRep = 0;
 	bool enPassant = false;
+
+	free(board);
 	return 0;
 }
