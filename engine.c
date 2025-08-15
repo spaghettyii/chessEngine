203185a1d49a3a07d8c053636733cdd9336ad399
spaghettyii <hyunwooyi2@gmail.com>
2025-08-15 16:23:35 -0300

    initial commit/made some basic stuff for representation of the board

---
+++ engine.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int* board_t;
+
+enum side {WHITE, BLACK}; 
+
+void initialize(board_t board)
+{
+
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	int *board = calloc(120, sizeof(int));
+	int fiftyMoves = 0;
+	int threeRep = 0;
+	bool enPassant = false;
+	return 0;
+}
